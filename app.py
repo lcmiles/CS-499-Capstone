a4de8a81d75bd86f6e16b635391692ae02b28c98
Logan Miles <lcmiles@uab.edu>
2025-03-25 14:22:48 -0500

    commented out db rebuild

--- app.py
+++ app.py
@@ -437,7 +437,7 @@ def utility_processor():
 
 if __name__ == "__main__":
     # uncomment line to rebuild sql db with next deployment
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
+    # with app.app_context():
+    #     db.drop_all()
+    #     db.create_all()
     app.run(host="0.0.0.0", port=8080)
